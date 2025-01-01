28e9bf64af9b2151f99baf50233a17c1f4976572
Ujjwal Bharti <ujjwalbharti111@gmail.com>
2025-01-01 23:16:53 +0530

    fix: redirect issue

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -1,22 +1,20 @@
 import { useShoppingCart } from '@/context/ShopingCartContext';
 import React from 'react';
 
-
 const CheckoutForm = () => {
+  const cartInfo = useShoppingCart();
 
-    const cartInfo = useShoppingCart();
-  
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     cartInfo.checkout();
-    (event.target as HTMLFormElement).submit();
+    window.location.href = '/';
   };
 
   return (
-    <form action="/" method="" onSubmit={handleSubmit}>
-      <button 
-        type="submit" 
-        className='block bg-gray-700 text-white text-sm sm:text-xl p-2 sm:p-3 rounded-lg'>
+    <form onSubmit={handleSubmit}>
+      <button
+        type="submit"
+        className="block bg-gray-700 text-white text-sm sm:text-xl p-2 sm:p-3 rounded-lg">
         Checkout
       </button>
     </form>
