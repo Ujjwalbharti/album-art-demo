c18361e0a2694ed0ec527a23f51d3242380e5df3
Ujjwal Bharti <ujjwalbharti111@gmail.com>
2025-01-04 22:48:11 +0530

    fix: added link in cart info page

--- src/components/CheckoutProductInfo.tsx
+++ src/components/CheckoutProductInfo.tsx
@@ -2,6 +2,7 @@ import { Product, ProductCheckoutInfo } from "@/model/models";
 import { ProductUtils } from "@/utils/utils";
 import Image from "next/image";
 import BuyButton from "./BuyButton";
+import Link from "next/link";
 
 interface CheckoutProps {
     productInfo: ProductCheckoutInfo;
@@ -11,13 +12,15 @@ const CheckoutProductInfo: React.FC<CheckoutProps> = ({ productInfo }) => {
     const product = ProductUtils.getProductById(productInfo.id)
     return (
         <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center border border-gray-700 m-1 sm:m-3 w-[98%] sm:w-full">
-            <Image
-                src={`/images/albums/${product?.imageName}`}
-                alt={product?.title || ""}
-                className='object-contain w-[50%] sm:w-[30%] lg:w-[10%] p-2'
-                width={100}
-                height={100}
-            />
+            <Link href={`/catalog/product-info/${product?._id}`}>
+                <Image
+                    src={`/images/albums/${product?.imageName}`}
+                    alt={product?.title || ""}
+                    className='object-contain w-[50%] sm:w-[30%] lg:w-[10%] p-2'
+                    width={100}
+                    height={100}
+                />
+            </Link>
             <div className="flex flex-col justify-center sm:w-[50%] p-3 text-lg font-sans">
                 <span className="block text-gray-600 font-bold">{product?.title}</span>
                 <span className="block font-extrabold">{`$${product?.price}`}</span>
