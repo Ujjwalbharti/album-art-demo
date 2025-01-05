e470cdd1fac10838a5fae75977fa6645818e47e1
Ujjwal Bharti <ujjwalbharti111@gmail.com>
2025-01-05 17:46:05 +0530

    fix: updated cart display

--- src/components/CheckoutProductInfo.tsx
+++ src/components/CheckoutProductInfo.tsx
@@ -1,3 +1,4 @@
+
 import { Product, ProductCheckoutInfo } from "@/model/models";
 import { ProductUtils } from "@/utils/utils";
 import Image from "next/image";
@@ -11,22 +12,24 @@ interface CheckoutProps {
 const CheckoutProductInfo: React.FC<CheckoutProps> = ({ productInfo }) => {
     const product = ProductUtils.getProductById(productInfo.id)
     return (
-        <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center border border-gray-700 m-1 sm:m-3 w-[98%] sm:w-full">
-            <Link href={`/catalog/product-info/${product?._id}`}>
-                <Image
-                    src={`/images/albums/${product?.imageName}`}
-                    alt={product?.title || ""}
-                    className='object-contain m-2'
-                    width={150}
-                    height={150}
-                />
-            </Link>
-            <div className="flex flex-col justify-center sm:w-[45%] p-2 text-lg font-sans">
-                <span className="block text-gray-600 font-bold">{product?.title}</span>
-                <span className="block font-extrabold">{`$${product?.price}`}</span>
-                <span className="block text-gray-500">{product?.description}</span>
+        <div className="flex flex-col sm:flex-row items-start sm:items-center border border-gray-700 m-1 sm:m-3 w-[98%] sm:w-full">
+            <div className="flex justify-start gap-3 sm:gap-5 w-full sm:w-[80%]">
+                <Link href={`/catalog/product-info/${product?._id}`} className="block m-2 w-[80px] h-[80px] sm:w-[120px] sm:h-[100px] md:w-[150px] md:h-[120px] lg:w-[150px] lg:h-[150px]">
+                    <Image
+                        src={`/images/albums/${product?.imageName}`}
+                        alt={product?.title || ""}
+                        className='object-contain  w-full h-full'
+                        width={50}
+                        height={50}
+                    />
+                </Link>
+                <div className="flex flex-col justify-start p-2 text-lg font-sans w-[70%]">
+                    <span className="block text-gray-600 font-bold">{product?.title}</span>
+                    <span className="block font-extrabold">{`$${product?.price}`}</span>
+                    <span className="sm:block text-gray-500 hidden">{product?.description}</span>
+                </div>
             </div>
-            <div className="flex sm:w-[30%] p-2 items-center justify-center text-white font-extrabold text-xl font-mono">
+            <div className="flex sm:w-[20%] p-2 items-center justify-center text-white font-extrabold text-xl font-mono">
                 <BuyButton product={product as Product} />
             </div>
         </div>
